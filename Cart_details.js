0f93193c9548c29e1fe9c55a61b4e96e880d4bb1
Nishtha-Goyal <88899093+Nishtha-Goyal@users.noreply.github.com>
2021-09-08 22:50:23 +0530

    Update Cart_details.js

--- Cart_details.js
+++ Cart_details.js
@@ -2,6 +2,14 @@ let items=[]
 
 function addItem(img,name,price,quantity=1)
 {
+    for( let i=0;i<items.length;i++)
+     {
+         if(items[i].name==name)
+         {
+             window.alert("Item Already added in the Cart");
+             return;
+         }
+     }
      items.push(
          {
              img:"images/"+img,
@@ -11,6 +19,18 @@ function addItem(img,name,price,quantity=1)
          }
      )
      window.alert("Item added Successfully");
+}
+
+function deleteItem(name)
+{
+    for( let i=0;i<items.length;i++)
+     {
+         if(items[i].name==name)
+         {
+             items.splice(i,1);
+         }
+     }
+     showCart();
 }
 
 function showCart(){
@@ -23,10 +43,10 @@ function showCart(){
     }
     let cart=`<table><tr>
            
-<td></td>
-<td>Item Name</td>
-<td>Quantity</td>
-<td>Price</td>
+<td style="font-size: 35px;background-color:black;color:white;">ITEM</td>
+<td style="font-size: 35px;background-color:black;color:white;">ITEM NAME</td>
+<td style="font-size: 35px;background-color:black;color:white;">QUANTITY</td>
+<td style="font-size: 35px;background-color:black;color:white;">PRICE</td>
 </tr>`
 let sum=0
 for( let i=0;i<items.length;i++)
@@ -35,13 +55,53 @@ for( let i=0;i<items.length;i++)
     <tr>
         <td><img src=${items[i].img} class="img" ></img></td>
         <td>${items[i].name}</td>
-        <td>${items[i].quantity}</td>
-        <td>${items[i].price}</td>
+        <td><i class="fa fa-plus-square" aria-hidden="true" onclick="increaseItem('${items[i].name}')"></i>  ${items[i].quantity}  <i class="fa fa-minus-square" aria-hidden="true" onclick="decreaseItem('${items[i].name}')"></i></td>
+        <td>${items[i].price*items[i].quantity}</td>
     </tr>  
   `
-    sum+=items[i].price
+    sum+=(items[i].quantity*items[i].price);
 }
-cart+=`<tr><td colspan="3">Total Amount</td><td>${sum}</td></tr>`
+// cart+=`<hr style="height:2px;border-width:0;color:black;background-color:gray">`
+cart+=`<tr><td colspan="3" style="color:black;"><b>Total Amount</td><td >${sum}</td></b></tr>`
 cart+="</table>"
 document.getElementById("Cart").innerHTML=cart
+}
+function increaseItem(nam)
+{
+    for( let i=0;i<items.length;i++)
+     {
+        if(items[i].name===nam)
+        {
+            if(items[i].quantity>=4)
+            {
+                window.alert("Cannot Add More Items");
+                break;
+            }
+            items[i].quantity++;
+        }
+     }
+     showCart();
+}
+function decreaseItem(nam)
+{
+    for( let i=0;i<items.length;i++)
+     {
+        if(items[i].name===nam)
+        {
+            if(items[i].quantity==1)
+            {
+                deleteItem(nam);
+                return;
+            }
+            // if(items[i].quantity<=1)
+            // {
+            //     items[i].quantity=1;
+            // }
+            else
+            {
+            items[i].quantity--;
+            }
+        }
+     }
+     showCart();
 }
